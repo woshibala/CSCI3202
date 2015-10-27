5431e09b44218f2d543e1b051744a01e4e265807
woshibala <yang.song@colorado.edu>
2015-10-26 21:14:18 -0600

    add professor's .py

---
+++ Assignment6/getoptExample.py
@@ -0,0 +1,43 @@
+import getopt, sys
+
+def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "m:g:j:p:")
+    except getopt.GetoptError as err:
+        # print help information and exit:
+        print str(err) # will print something like "option -a not recognized"
+        sys.exit(2)
+    for o, a in opts:
+		if o in ("-p"):
+			print "flag", o
+			print "args", a
+			print a[0]
+			print float(a[1:])
+			#setting the prior here works if the Bayes net is already built
+			#setPrior(a[0], float(a[1:])
+		elif o in ("-m"):
+			print "flag", o
+			print "args", a
+			print type(a)
+			#calcMarginal(a)
+		elif o in ("-g"):
+			print "flag", o
+			print "args", a
+			print type(a)
+			'''you may want to parse a here and pass the left of |
+			and right of | as arguments to calcConditional
+			'''
+			p = a.find("|")
+			print a[:p]
+			print a[p+1:]
+			#calcConditional(a[:p], a[p+1:])
+		elif o in ("-j"):
+			print "flag", o
+			print "args", a
+		else:
+			assert False, "unhandled option"
+		
+    # ...
+
+if __name__ == "__main__":
+    main()
