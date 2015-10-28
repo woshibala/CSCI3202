6bce56799157ef990010219cf43a00b0966870ad
woshibala <songyang119@126.com>
2015-10-28 10:42:24 -0600

    new

--- Assignment6/getoptExample.py
+++ Assignment6/getoptExample.py
@@ -43,7 +43,7 @@ def input():
 def main():
 	global o,a,graph
 	result = None
-	graph = {'p':[[],["c"],[0.9,0.1]],'s':[[],["c"],[0.3,0.7]],'c':[["p","s"],["x","d"],[0.05,0.02,0.03,0.001]],'x':[["c"],[],[0.9,0.2]],'d':[["c"],[],[0.65,0.3]]};
+	graph = {'p':[[],["c"],[0.1,0.9]],'s':[[],["c"],[0.3,0.7]],'c':[["p","s"],["x","d"],[0.05,0.02,0.03,0.001]],'x':[["c"],[],[0.9,0.2]],'d':[["c"],[],[0.65,0.3]]};
 	#graph["c"][2] => p=h&s=t 0.05; hf 0.02; lt 0.03; lf 0.001                                                         p(x|c)=0.9 p(x|~c)=0.2     p(d|c)=0.65 p(d|~c)=0.3
  	given = {"p":0.9}
 	#A:[[parents],[children]]
@@ -57,36 +57,64 @@ def main():
 			print marginal2(A,"c")
 		else:#cancer
 			print marginal3(A,"p","s")
-	if o == "-g":
-		A = a.split("/",1)[0]
-		B = a.split("/",1)[1]
-		conditional()
-		
+	elif o == "-g":
+		A = a.split("/")[0]
+		B = a.split("/")[1]
+		print A,B
+		if A == B:
+			print 1
+			return 
+		if len(graph[A][0]) == 0:#A is pollution and smoker
+			if B in graph[A][1]:#B is cancer ?intercausal
+				print conditional4(A,B) #s|c p|s
 
+		elif len(graph[A][1]) == 0:#A is X-ray and Dyapnose
+			print conditional2(A,B)
+		else:#A is cancer 
+			if B in graph[A][0]:#B is cancer's parent
+				print conditional5(A,B)
 
-
-
+def conditional4(A,B):
+	print A,B #A=s B=c
+	print conditional5(B,A),marginal1(A),marginal3("c","p","s")
+	result = conditional5(B,A)*marginal1(A)/marginal3("c","p","s")
+	return result
 def conditional2(A,B):
-	print A
-	print B
-	if B[1:] in graph[A][0] or B in graph[A][0]:#diagnostic c
+	print "c2",A
+	print "c2",B
+	if B[1:] in graph[A][0] or B in graph[A][0]:#diagnostic c x|c
 		print A," is ",B,"'parent"
 		if B == "c":
 			return graph[A][2][0]
 		else:
 			return graph[A][2][1]
-	elif A in graph[B][1]:#predictive
-		return 
+	elif A in graph[B][1]:#predictive c|s
+		print "here"
+
+def conditional5(A,B):
+	print "c5",A
+	print "c5",B
+	if B[1:] in graph[A][0] or B in graph[A][0]:#diagnostic c P(c|s) = P(c|p,s)P(p)+P(c|~p,s)P(~p)
+		print A," is ",B,"'parent"
+		if  B == "s":
+			return conditional3(A,"p",B)*marginal1("p")+conditional3(A,"~p",B)*(1-marginal1("p"))
+		elif B == "p":#P(c|p) = P(c|p,s)P(s)+P(c|p,~s)P(~s)
+		 	print conditional3(A,B,"s"),marginal1("s"),conditional3(A,"~s",B),(1-marginal1("s"))
+			return conditional3(A,B,"s")*marginal1("s")+conditional3(A,"~s",B)*(1-marginal1("s"))
+
+			
+	elif A in graph[B][1]:#predictive c|s
+		print "here"
 def conditional3(A,B,C):#P(A|B C)
 	#print A,B,C
 	if B == "~p" and C == "s":
-		return graph[A][2][0]
-	elif B == "~p" and C == "~s":
-		return graph[A][2][1]
-	elif B == "p" and C == "s":
 		return graph[A][2][2]
-	else:
+	elif B == "~p" and C == "~s":
 		return graph[A][2][3]
+	elif B == "p" and C == "s":
+		return graph[A][2][0]
+	else:
+		return graph[A][2][1]
 
 
 
