de77703d170ef9fcaed14f64a1b33aa623c975b3
woshibala <yang.song@colorado.edu>
2015-10-28 16:06:35 -0600

    Done

--- Assignment6/a.py
+++ Assignment6/a.py
@@ -44,7 +44,7 @@ def input():
 def main():
 	global o,a,graph
 	result = None
-	graph = {'p':[[],["c"],[0.1,0.9]],'s':[[],["c"],[0.3,0.7]],'c':[["p","s"],["x","d"],[0.05,0.02,0.03,0.001]],'x':[["c"],[],[0.9,0.2]],'d':[["c"],[],[0.65,0.3]]};
+	graph = {'p':[[],["c"],[0.1,0.9],["x","d"]],'s':[[],["c"],[0.3,0.7],["x","d"]],'c':[["p","s"],["x","d"],[0.05,0.02,0.03,0.001]],'x':[["c"],[],[0.9,0.2]],'d':[["c"],[],[0.65,0.3]]};
 	#graph["c"][2] => p=h&s=t 0.05; hf 0.02; lt 0.03; lf 0.001                                                         p(x|c)=0.9 p(x|~c)=0.2     p(d|c)=0.65 p(d|~c)=0.3
  	given = {"p":0.9}
 	#A:[[parents],[children]]
@@ -67,19 +67,90 @@ def main():
 			return 
 		if len(graph[A][0]) == 0:#A is pollution and smoker
 			if B in graph[A][1]:#B is cancer ?intercausal
+				print "f"
 				print conditional4(A,B) #s|c p|s
-
+			elif B in graph[A][3]:#B is x-ray or dyapmose 
+			#s|d
+				print "!!!!!!!!!!!!!!!111111"
+				print conditional8(A,B)
 		elif len(graph[A][1]) == 0:#A is X-ray and Dyapnose
-			print conditional2(A,B)
+			print "h"
+			if A in graph[B][3]:#d|s d|p x|s x|p
+				print A,B
+				print conditional6(A,B)
+			else:
+				print conditional2(A,B)
 		else:#A is cancer 
 			if B in graph[A][0]:#B is cancer's parent
 				print conditional5(A,B)
 
 def conditional4(A,B):
-	print A,B #A=s B=c
-	print conditional5(B,A),marginal1(A),marginal3("c","p","s")
 	result = conditional5(B,A)*marginal1(A)/marginal3("c","p","s")
 	return result
+
+def conditional8(A,B):
+	print "c8",A,B
+	print conditional6(B,A),marginal1(A),marginal1(B)
+	#P(d|s)P(s)/P(d|s)P(s)+P(d|~s)P(~s)
+	result = conditional6(B,A)*marginal1(A)/(conditional6(B,A)*marginal1(A)+conditional6(B,"~"+A)*(1-marginal1(A)))
+	return result
+def conditional6(A,B):#d|s d|p x|s x|p
+	print "c6",A,B
+	if A == "d" and B == "s":
+		r1 = joint4(A,B,"c","p")
+		r2 = joint3(B,"c","p")
+		return r1/r2
+	elif A == "x" and B =="s":
+		r1 = joint4(A,B,"c","p")
+		r2 = joint3(B,"c","p")
+		return r1/r2
+	elif A == "d" and B == "~s":
+		r1 = joint4n(A,B[1:],"c","p")
+		r2 = joint3n(B[1:],"c","p")
+		return r1/r2
+
+def joint3n(B,C,D):
+	print "j3",B,C,D
+	r1 = conditional3(C,D,"~"+B)*marginal1(D)*(1-marginal1(B))
+	r2 = conditional3(C,"~"+D,"~"+B)*(1-marginal1(D))*(1-marginal1(B))
+	r3 = (1-conditional3(C,D,"`"+B))*marginal1(D)*(1-marginal1(B))
+	r4 = (1-conditional3(C,"~"+D,"~"+B))*(1-marginal1(D))*(1-marginal1(B))
+	print "j3",r1+r2+r3+r4
+	return r1+r2+r3+r4
+def joint4n(A,B,C,D):#P(d,s,C,P)
+	#P(d|c) P(x|c)
+	print "j4",A,B,C,D
+	r1 = conditional2(A,C)*conditional3(C,D,"~"+B)*marginal1(D)*(1-marginal1(B))
+	print r1
+	r2 = conditional2(A,C)*conditional3(C,"~"+D,"~"+B)*(1-marginal1(D))*(1-marginal1(B))
+	print r2
+	r3 = conditional2(A,"~"+C)*(1-conditional3(C,D,"~"+B))*marginal1(D)*(1-marginal1(B))
+	print r3
+	r4 = conditional2(A,"~"+C)*(1-conditional3(C,"~"+D,"~"+B))*(1-marginal1(D))*(1-marginal1(B))
+	print r4
+	print "j4",r1+r2+r3+r4
+	return r1+r2+r3+r4
+def joint3(B,C,D):
+	print "j3",B,C,D
+	r1 = conditional3(C,D,B)*marginal1(D)*marginal1(B)
+	r2 = conditional3(C,"~"+D,B)*(1-marginal1(D))*marginal1(B)
+	r3 = (1-conditional3(C,D,B))*marginal1(D)*marginal1(B)
+	r4 = (1-conditional3(C,"~"+D,B))*(1-marginal1(D))*marginal1(B)
+	print "j3",r1+r2+r3+r4
+	return r1+r2+r3+r4
+def joint4(A,B,C,D):#P(d,s,C,P)
+	#P(d|c) P(x|c)
+	print "j4",A,B,C,D
+	r1 = conditional2(A,C)*conditional3(C,D,B)*marginal1(D)*marginal1(B) 
+	print r1
+	r2 = conditional2(A,C)*conditional3(C,"~"+D,B)*(1-marginal1(D))*marginal1(B)
+	print r2
+	r3 = conditional2(A,"~"+C)*(1-conditional3(C,D,B))*marginal1(D)*marginal1(B)
+	print r3
+	r4 = conditional2(A,"~"+C)*(1-conditional3(C,"~"+D,B))*(1-marginal1(D))*marginal1(B)
+	print r4
+	print "j4",r1+r2+r3+r4
+	return r1+r2+r3+r4
 def conditional2(A,B):
 	print "c2",A
 	print "c2",B
@@ -87,10 +158,12 @@ def conditional2(A,B):
 		print A," is ",B,"'parent"
 		if B == "c":
 			return graph[A][2][0]
-		else:
+		else: #B == "~c"
 			return graph[A][2][1]
 	elif A in graph[B][1]:#predictive c|s
 		print "here"
+	else: 
+		print "c2e"
 
 def conditional5(A,B):
 	print "c5",A
@@ -103,22 +176,17 @@ def conditional5(A,B):
 		 	print conditional3(A,B,"s"),marginal1("s"),conditional3(A,"~s",B),(1-marginal1("s"))
 			return conditional3(A,B,"s")*marginal1("s")+conditional3(A,"~s",B)*(1-marginal1("s"))
 
-			
-	elif A in graph[B][1]:#predictive c|s
-		print "here"
 def conditional3(A,B,C):#P(A|B C)
 	#print A,B,C
-	if B == "~p" and C == "s":
-		return graph[A][2][2]
-	elif B == "~p" and C == "~s":
-		return graph[A][2][3]
-	elif B == "p" and C == "s":
-		return graph[A][2][0]
-	else:
-		return graph[A][2][1]
-
-
-
+	if A =="c":
+		if B == "~p" and C == "s":
+			return graph[A][2][2]
+		elif B == "~p" and C == "~s":
+			return graph[A][2][3]
+		elif B == "p" and C == "s":
+			return graph[A][2][0]
+		else:
+			return graph[A][2][1]
 
 def marginal1(A):
 	global graph
@@ -142,15 +210,8 @@ def marginal3(A,B,C):
 
 
 
-
-
-
-
-
 def run():
 	input()
 	main()
 
 run()
-Status API Training Shop Blog About Pricing
-© 2015 GitHub, Inc. Terms Privacy Security Contact Help
